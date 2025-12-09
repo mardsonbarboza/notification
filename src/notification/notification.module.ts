95498481eb53b748b04476fee73329de57771d3f
mardson barboza <mardson49@gmail.com>
2025-12-09 15:50:50 -0300

    fix: resolve merge conflicts and restore PrismaModule import in NotificationModule

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
-=======
-import { PrismaModule } from '../prisma/prisma.module';
-import { EventsModule } from '../events/events/events.module';
->>>>>>> 5f4f77808fab4491ae31c01e82714619d02675a4
+
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { EventsModule } from '../events/events.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 
 @Module({
-<<<<<<< HEAD
-  imports: [], // ← ADICIONE ISSO
-=======
   imports: [PrismaModule, EventsModule],
->>>>>>> 5f4f77808fab4491ae31c01e82714619d02675a4
   providers: [NotificationService],
   controllers: [NotificationController],
   exports: [NotificationService],
